706f1624006c9a825f7aab3b9df621a9cb871c0d
Daniel Willard <dwillar4@testium.cs.uoregon.edu>
2023-02-22 22:03:41 -0800

    algothim implemented

--- brevets/acp_times.py
+++ brevets/acp_times.py
@@ -13,6 +13,21 @@ import arrow
 #  same arguments.
 #
 
+MIN_SPEED = {
+        0: 15,
+        200: 15,
+        400: 15,
+        600: 11.428,
+        1000: 13.333
+        }
+
+MAX_SPEED = {
+        0: 34,
+        200: 32,
+        400: 30,
+        600: 28,
+        1000: 26
+        }
 
 def open_time(control_dist_km, brevet_dist_km, brevet_start_time):
     """
@@ -26,7 +41,20 @@ def open_time(control_dist_km, brevet_dist_km, brevet_start_time):
        An arrow object indicating the control open time.
        This will be in the same time zone as the brevet start time.
     """
-    return arrow.now()
+    # Error catching control longer then total race distance
+    if control_dist_km > brevet_dist_km:
+        return None
+    # Error catching if control distance is 0 or less start time is the race start
+    if control_dist_km <= 0:
+        return brevet_start_time
+    if control_dist_km < 0:
+        return None
+
+    ride_time = (control_dist_km/ MAX_SPEED[min(control_dist_km, 1000)]) * 60
+    ride_hours = math.floor(ride_time/60)
+    ride_minutes = math.floor(ride_time % 60)
+
+    return brevet_start_time.shift(hours= ride_hours, minutes= ride_minutes)
 
 
 def close_time(control_dist_km, brevet_dist_km, brevet_start_time):
@@ -41,4 +69,16 @@ def close_time(control_dist_km, brevet_dist_km, brevet_start_time):
        An arrow object indicating the control close time.
        This will be in the same time zone as the brevet start time.
     """
-    return arrow.now()
+
+    # Error catching control longer then total race distance
+    if control_dist_km > brevet_dist_km:
+        return None
+    # Error catching if control distance is 0 or less start time is the race start
+    if control_dist_km <= 0:
+        return brevet_start_time.shift(hours=1)
+
+    ride_time = (control_dist_km/ MIN_SPEED[min(control_dist_km, 1000)]) * 60
+    ride_hours = math.floor(ride_time/60)
+    ride_minutes = math.floor(ride_time % 60)
+
+    return brevet_start_time.shift(hours= ride_hours, minutes= ride_minutes)
